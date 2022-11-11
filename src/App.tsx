3c6a16ffb8b9672f1490899f677d98c3aa665ec7
magarteem <fmf311@mail.ru>
2022-11-11 04:44:09 +0200

    add calendar birthday \ add logic change profile \ add logic change and push in stor new data profile-3

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { AppRouter } from "./core/router/AppRouter";
 import "./styles/index.scss";
 
 export const App = () => {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+ return (
+  <div className="App">
+   <AppRouter />
+  </div>
+ );
 };
